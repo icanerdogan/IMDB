60c3a6e57ade01f780e3f4718fd48f8264337468
icanerdogan <ibrahim4545erdogan@gmail.com>
2022-03-23 14:52:36 +0300

    Finished MovieActivity

--- app/src/main/java/com/icanerdogan/imdb/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/icanerdogan/imdb/presentation/movie/MovieActivity.kt
@@ -2,12 +2,15 @@ package com.icanerdogan.imdb.presentation.movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.icanerdogan.imdb.R
 import com.icanerdogan.imdb.databinding.ActivityMovieBinding
 import com.icanerdogan.imdb.presentation.dependencyinjection.Injector
 import javax.inject.Inject
@@ -49,6 +52,36 @@ class MovieActivity : AppCompatActivity() {
             }else{
                 binding.progressBarMovie.visibility = View.GONE
                 Toast.makeText(applicationContext, "No Data Avaliable!", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    // UPDATE BUTTON CLICK
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.update, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_update -> {
+                updateMovies()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    private fun updateMovies(){
+        binding.progressBarMovie.visibility = View.VISIBLE
+        val response = movieViewModel.updateMovies()
+        response.observe(this,  Observer {
+            if (it != null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.progressBarMovie.visibility = View.GONE
+            }else{
+                binding.progressBarMovie.visibility = View.GONE
             }
         })
     }
